5e7794312d370ce5b74238788592a1e66e021aaa
Tryggvi <tryggviola@gmail.com>
2022-01-30 19:25:40 +0000

    add assignments to home page + minor styling

--- client/src/routes/home/Home.js
+++ client/src/routes/home/Home.js
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 
 import Header from "../../components/header/Header";
 
+import "./Home.scss";
+
 export default function Home() {
 	const [articles, setArticles] = useState([]);
+	const [assignments, setAssignments] = useState([]);
 	useEffect(() => {
 		getArticles();
+		getAssignments();
 	}, []);
 
 	const getArticles = async () => {
@@ -19,23 +23,47 @@ export default function Home() {
 		setArticles(publishedArticles);
 	};
 
+	const getAssignments = async () => {
+		const response = await fetch(
+			`${process.env.REACT_APP_API_SERVER}/assignments`
+		);
+		const data = await response.json();
+
+		setAssignments(data);
+	};
+
 	return (
-    <div>
+		<div>
 			<Header />
-			<div className="article-body">
-        <div className="title">
-				  <h1>Articles</h1>
-			  </div>
-			  {articles?.map((article) => (
-				  <div className="article" key={article._id}>
-					  <div>
-						  <Link to={`/article/${article._id}`}>{article?.title}</Link>
-						  <h3>{article?.subtitle}</h3>
-              <h3>{article?.author}</h3>
-					  </div>
-				  </div>
-			))}
-      </div>
+			<div className="section">
+				<div className="section__header">
+					<h1>Articles</h1>
+				</div>
+				{articles?.map((article) => (
+					<div className="section__body" key={article._id}>
+						<Link
+							className="section__body__title"
+							to={`/article/${article._id}`}
+						>
+							{article?.title}
+						</Link>
+						<h3>{article?.subtitle}</h3>
+						<h3>{article?.author}</h3>
+					</div>
+				))}
+			</div>
+
+			<div className="section">
+				<div className="section__header">
+					<h1>Assignments</h1>
+				</div>
+				{assignments?.map((assignment) => (
+					<div className="section__body" key={assignment._id}>
+						<h3 className="section__body__title">{assignment?.title}</h3>
+						<h3>{assignment?.author}</h3>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
