9f6f7a6d012f00de4bd65760990aa5a084479f37
thorbergurharaldsson <thorbergur@haraldssonprod.is>
2022-03-24 14:24:14 +0000

    Assignments page now showing better info

--- client/src/routes/studio/assignments/Assignments.js
+++ client/src/routes/studio/assignments/Assignments.js
@@ -6,7 +6,7 @@ import { horsemernAPI } from "../../../utils/api";
 
 import "./Assignments.scss";
 
-function Assignments(params) {
+function Assignments() {
   const { user } = useAuth();
   const [assignments, setAssignments] = useState([]);
 
@@ -19,7 +19,7 @@ function Assignments(params) {
     const data = (await response).data;
     console.log(data);
     const filteredAssignments = data.filter(
-      (assignment) => assignment.author == user.name
+      (assignment) => assignment.author === user.name
     );
 
     setAssignments(filteredAssignments);
@@ -33,9 +33,10 @@ function Assignments(params) {
       {assignments?.map((assignment) => (
         <div className="assignment" key={assignment._id}>
           <div>
-            <h2>{assignment?.title}</h2>
-            <h3>{assignment.author}</h3>
-            <Markdown>{assignment?.description}</Markdown>
+            <h2>{assignment?.assignmentTitle}</h2>
+            <h3>{assignment?.moduleTitle}</h3>
+            <Markdown>{assignment?.comment}</Markdown>
+            <a href={assignment?.url}>{assignment?.url}</a>
           </div>
         </div>
       ))}
