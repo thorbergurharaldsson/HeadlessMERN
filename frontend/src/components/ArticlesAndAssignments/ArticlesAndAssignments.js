56b39d950641850538fe3f443936c5b9c1ab7739
thorbergurharaldsson <thorbergur@haraldssonprod.is>
2022-03-29 16:00:54 +0000

    added time to read and only loading first 40 words

--- frontend/src/components/ArticlesAndAssignments/ArticlesAndAssignments.js
+++ frontend/src/components/ArticlesAndAssignments/ArticlesAndAssignments.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 import { horsemernAPI } from "../../utils/api";
 import dateParts from "../../utils/dateParts";
 import styles from "../../styles/index.module.scss";
@@ -9,8 +10,7 @@ const ArticlesAndAssignments = () => {
   const concatenate = async (path1, path2) => {
     const assignments = await horsemernAPI.get(`${path1}`);
     const articles = await horsemernAPI.get(`${path2}`);
-    // console.log(assignments.data);
-    // console.log(articles.data.data);
+
     setConcatArr(
       assignments.data.concat(articles.data.data).sort((a, b) => {
         return (
@@ -33,6 +33,22 @@ const ArticlesAndAssignments = () => {
     return <div>Loading...</div>;
   }
 
+  // calculate time to read
+  const timeToRead = (text) => {
+    const words = text.split(" ");
+    const wordsPerMinute = words.length / (60 * 2);
+    return wordsPerMinute < 1 ? 1 : Math.round(wordsPerMinute);
+  };
+
+  // load first 40 words of article
+  const loadFirst40Words = (content) => {
+    const text = content;
+    const words = text.split(" ");
+    const first40Words = words.slice(0, 40);
+    const first40WordsJoined = first40Words.join(" ");
+    return first40WordsJoined;
+  };
+
   return (
     <>
       <div className={styles.container}>
@@ -51,7 +67,7 @@ const ArticlesAndAssignments = () => {
                     </p>
                     <h2>{entry.assignmentTitle}</h2>
                     <h4>{entry.moduleTitle}</h4>
-                    <p dangerouslySetInnerHTML={{ __html: entry.comment }}></p>
+                    <ReactMarkdown>{entry.comment}</ReactMarkdown>
                     <button onClick={(e) => getID(e.target.value)}>
                       <option value={entry._id}>...</option>
                     </button>
@@ -67,13 +83,21 @@ const ArticlesAndAssignments = () => {
                     </p>
                     <h2>{entry.title}</h2>
                     <h4>{entry.subtitle}</h4>
-                    <p> {entry.content}</p>
+                    <ReactMarkdown>
+                      {loadFirst40Words(entry.content)}
+                    </ReactMarkdown>
                     <button onClick={(e) => getID(e.target.value)}>
                       <option value={entry._id}>...</option>
                     </button>
                     <div className={styles.tagContainer}>
                       <button className={styles.buttonTag}>Tag</button>
                       <button className={styles.buttonTag}>Tag</button>
+                      <p
+                        className={styles.pSmall}
+                        style={{ paddingLeft: "3rem" }}
+                      >
+                        {timeToRead(entry.content)}m to read
+                      </p>
                     </div>
                   </div>
                 )}
